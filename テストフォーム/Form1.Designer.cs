534e2d561dfffed5d64637b4a6068d5279125441
kaba30 <teruyoshi_kaibara_2016@la-bit.sakura.ne.jp>
2020-02-24 13:24:12 +0900

    - C#日本語化プロジェクト（実験用）と、テストフォームプロジェクト（試験用）を追加。

---
+++ テストフォーム/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace テストフォーム
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.リストボックス1 = new Ｃシャープ日本語化.リストボックス();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // リストボックス1
+            // 
+            this.リストボックス1.FormattingEnabled = true;
+            this.リストボックス1.ItemHeight = 12;
+            this.リストボックス1.Location = new System.Drawing.Point(108, 109);
+            this.リストボックス1.Name = "リストボックス1";
+            this.リストボックス1.Size = new System.Drawing.Size(120, 88);
+            this.リストボックス1.TabIndex = 0;
+            this.リストボックス1.ドロップ許可 = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.リストボックス1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Ｃシャープ日本語化.リストボックス リストボックス1;
+        private System.Windows.Forms.Button button1;
+
+    }
+}
+
---
+++ Ｃシャープ日本語化/リストボックス.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Ｃシャープ日本語化
+{
+    public class リストボックス : ListBox
+    {
+        public リストボックス()
+            : base()
+        {
+        }
+
+        public void sub()
+        {
+            this.FindString("AA");
+            //this.
+        }
+
+        // ↓この属性は、DLLの外側（使用側）からは見えなくなる。自分自身のフォームからはまだ見える
+        //    参考URL：http://devlights.hatenablog.com/entry/20080418/p1
+        //　　　　　　http://acha-ya.cocolog-nifty.com/blog/2010/09/post-de60.html
+        // 　これを使えば、sealed→サブクラス　の手順を踏むまでもない。
+        // [Browsable(false)]                              // プロパティウィンドウ非表示
+        // [EditorBrowsable(EditorBrowsableState.Never)]   // インテリセンス非表示
+        [Browsable(false)] 
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override bool AllowDrop
+        {
+            get
+            {
+                return base.AllowDrop;
+            }
+            set
+            {
+                base.AllowDrop = value;
+            }
+        }
+
+        /// <summary>
+        /// aaa
+        /// </summary>
+        /// <remarks>
+        /// bbb
+        /// </remarks>
+        /// <example>
+        /// <code>
+        /// ccc
+        /// </code>
+        /// </example>
+        public virtual bool ドロップ許可 { set; get; }
+    }
+}
